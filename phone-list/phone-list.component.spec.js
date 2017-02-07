0df460080c63f03173d02b110be60dcf0d9fa3b9
Ilya Maksimenko <ilya.maksimenko@effective-soft.com>
2017-02-07 14:01:45 +0300

    Stage 6

--- phone-list/phone-list.component.spec.js
+++ phone-list/phone-list.component.spec.js
@@ -5,9 +5,17 @@ describe('phoneList', function () {
     beforeEach(module('phoneList'));
 
     describe('PhoneListController', function () {
-        it('should create a \'phones\' model with 3 phones', inject(function ($componentController) {
-            var ctrl = $componentController('phoneList');
-            expect(ctrl.phones.length).toBe(3);
+        
+        var ctrl;
+        beforeEach(inject(function ($componentController) {
+            ctrl = $componentController('phoneList');
         }));
+
+        it('should create a \'phones\' model with 3 phones', function () {
+            expect(ctrl.phones.length).toBe(3);
+        });
+        it('should set a default value for the \'orderProp\' model', function () {
+            expect(ctrl.orderProp).toBe('age');
+        });
     });
 });
