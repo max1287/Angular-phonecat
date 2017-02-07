6a4a20f0706e37a6a534cb68ab627f28decb245d
Ilya Maksimenko <ilya.maksimenko@effective-soft.com>
2017-02-07 15:13:01 +0300

    Stage 7

--- phone-list/phone-list.component.spec.js
+++ phone-list/phone-list.component.spec.js
@@ -4,16 +4,21 @@ describe('phoneList', function () {
 
     beforeEach(module('phoneList'));
 
-    describe('PhoneListController', function () {
-        
-        var ctrl;
-        beforeEach(inject(function ($componentController) {
+    describe('controller', function () {
+        var $httpBackend, ctrl;
+        beforeEach(inject(function ($componentController, _$httpBackend_) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json')
+                        .respond([{ name: 'Nexus S' }, { name: 'Motorola DROID' }]);
             ctrl = $componentController('phoneList');
         }));
 
-        it('should create a \'phones\' model with 3 phones', function () {
-            expect(ctrl.phones.length).toBe(3);
+        it('should create a \'phones\' property with 2 phones fetched with \'$http\'', function () {
+            expect(ctrl.phones).toBeUndefined();
+            $httpBackend.flush();
+            expect(ctrl.phones).toEqual([{ name: 'Nexus S' }, { name: 'Motorola DROID' }]);
         });
+
         it('should set a default value for the \'orderProp\' model', function () {
             expect(ctrl.orderProp).toBe('age');
         });
