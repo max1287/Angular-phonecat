ba7e34096e6f2d46fd3ee3141a455aa9e63c56e2
Ilya Maksimenko <ilya.maksimenko@effective-soft.com>
2017-02-08 13:49:16 +0300

    Stage 12

--- phone-detail/phone-detail.component.js
+++ phone-detail/phone-detail.component.js
@@ -7,8 +7,18 @@ angular.
         controller: ['$http', '$routeParams',
             function PhoneDetailController($http,$routeParams) {
                 var self = this;
+
+                self.setImage = function setImage(imageUrl) {
+                    self.mainImageUrl = imageUrl;
+                }
+
+                self.onDblclick = function onDblclick(imageUrl) {
+                    alert('You double-clicked image: ' + imageUrl);
+                };
+
                 $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
                     self.phone = response.data;
+                    self.setImage(self.phone.images[0]);
                 });
             }
             ]
--- phone-detail/phone-detail.component.spec.js
+++ phone-detail/phone-detail.component.spec.js
@@ -6,10 +6,15 @@ describe('phoneDetail', function () {
 
     describe('PhoneDetailController', function () {
         var $httpBackend, ctrl;
+        var xyzPhoneData = {
+            name: 'phone xyz',
+            images: ['image/url1.png', 'image/url2.png']
+        };
+
         beforeEach(inject(function ($componentController, _$httpBackend_, $routeParams) {
             $httpBackend = _$httpBackend_;
             $httpBackend.expectGET('phones/xyz.json')
-                        .respond({ name: 'phone xyz' });
+                        .respond(xyzPhoneData);
             $routeParams.phoneId = 'xyz';
             ctrl = $componentController('phoneDetail');
         }));
@@ -18,9 +23,11 @@ describe('phoneDetail', function () {
             expect(ctrl.phone).toBeUndefined();
 
             $httpBackend.flush();
-            expect(ctrl.phone).toEqual({ name: 'phone xyz' });
+            expect(ctrl.phone).toEqual(xyzPhoneData);
         });
 
     });
+
+
 
 });
